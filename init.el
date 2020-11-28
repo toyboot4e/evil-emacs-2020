d4bd4dd010dda8e43dad4978358da57803571666
toyboot4e <toyboot4e@gmail.com>
2020-11-29 00:38:44 +0900

    add elisp/

--- init.el
+++ init.el
@@ -3,3 +3,10 @@
 (setq user-full-name    "Vim 侍"
       user-mail-address "vim-samurai@gmail.com")
 
+;; allow loading symlinks
+(setq vc-follow-symlinks t)
+
+(load-file (concat user-emacs-directory "elisp/evil.el"))
+(load-file (concat user-emacs-directory "elisp/ide.el"))
+(load-file (concat user-emacs-directory "elisp/keymap.el"))
+
