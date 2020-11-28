376bdbe2dc06a450aa6cd15049af42d824b819b6
toyboot4e <toyboot4e@gmail.com>
2020-11-29 03:30:59 +0900

    add neotree keys

--- elisp/keymap.el
+++ elisp/keymap.el
@@ -221,9 +221,28 @@
 
 ;; ------------------------------ Leaders ------------------------------
 
+;; https://www.emacswiki.org/emacs/NeoTree
+(defun toy/neo-proj ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+        (neotree-show)
+        (if project-dir
+                (if (neo-global--window-exists-p)
+                        (progn
+                            (neotree-dir project-dir)
+                            (neotree-find file-name)))
+            (message "Could not find git project root."))))
+
 (evil-define-key 'normal 'global
     " :" #'counsel-M-x
     " ;" #'shell-command
+
+    " nn" #'toy/neo-proj
+    " nt" #'neotree-toggle
+    " nf" #'neotree-find
+    " nq" #'neotree-quick-look
     )
 
 ;; [f]ind
